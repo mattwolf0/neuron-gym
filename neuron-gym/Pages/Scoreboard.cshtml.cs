fe7562e6ab4c23bf7054b7fdfaeddfdbf6a244ad
WMatheist <wmatheist@protonmail.com>
2025-11-20 20:47:05 +0900

    Implement Simon Says game and add dedicated styles

---
+++ neuron-gym/Pages/Scoreboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace neuron_gym.Pages
+{
+    public class ScoreboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
